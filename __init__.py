3649bb35c1ff733b5ed5d1635dd134b8f3e5af98
LiuTaowen Tony <tony_t_liu@outlook.com>
2024-11-05 19:50:55 +0000

    no need to input __file__ in relative_path

--- __init__.py
+++ __init__.py
@@ -1,1 +1,1 @@
-import ml_utils.data, ml_utils.log_util, ml_utils.optim, ml_utils.job_queue
+import ml_utils.data, ml_utils.log_util, ml_utils.optim, ml_utils.job_queue, ml_utils.path
--- path.py
+++ path.py
@@ -1,4 +1,5 @@
 import pathlib
 
-def relative_path(file_path: str, underscore_file: str) -> str:
-    return (pathlib.Path(file_path).parent / underscore_file).as_posix()
+def relative_path(file_path: str) -> str:
+    underscore_file = eval("__file__")
+    return (pathlib.Path(underscore_file).parent / file_path).as_posix()
